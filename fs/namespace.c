b2872a2bb67c2ca604b960b9159bc745761d2f6b
Noxxx <apowertje@hotmail.com>
2016-12-07 04:33:22 +0100

    FS: Noatime default
    Signed-off-by: geiti94 <geiti94@me.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2637,8 +2637,9 @@ long do_mount(const char *dev_name, const char *dir_name,
 		goto dput_out;
 
 	/* Default to noatime unless overriden */
-	if (!(flags & MS_RELATIME))
+	if (!(flags & MS_RELATIME)) {
 		mnt_flags |= MNT_NOATIME;
+	}
 
 	/* Separate the per-mountpoint flags */
 	if (flags & MS_NOSUID)
