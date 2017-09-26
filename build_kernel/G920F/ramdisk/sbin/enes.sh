7bac956fa03d833cb37340fbe955bc872a28f185
minealex2244 <minealex2244@gmail.com>
2017-09-26 20:44:26 +0300

    Synapse: Added more live frequencies and fixed some things. Also credits to UpInTheAir and Noxxxious for Doze profiles

--- build_kernel/G920F/ramdisk/sbin/enes.sh
+++ build_kernel/G920F/ramdisk/sbin/enes.sh
@@ -28,10 +28,8 @@ mount -o rw,remount rootfs;
 
 # Fix permissions
 
-chmod 0666 /sys/devices/system/cpu/cpu4/cpufreq/interactive/param_index
-chmod 0666 /sys/devices/system/cpu/cpu4/cpufreq/interactive/cpu_util
-chmod 0666 /sys/devices/system/cpu/cpu0/cpufreq/interactive/cpu_util
 chmod 0666 /sys/devices/14ac0000.mali/dvfs_governor
+chmod 0666 /sys/module/workqueue/parameters/power_efficient
 
 # Synapse
 
