0381af34dec5219ae6e963d9bc0f7d8bdfd07903
enesuzun2002 <enesuzun200227@gmail.com>
2019-06-11 09:18:28 +0000

    ramdisk: fix perms

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@
 # -----
 export ARCH=arm64
 export SUBARCH=arm64
-export BUILD_CROSS_COMPILE=/home/enes_aosp002/android_prebuilts_linaro/bin/aarch64-linux-gnu-
+export BUILD_CROSS_COMPILE=/home/enes_aosp003/LineageOS/prebuilts/linaro/linux-x86/aarch64/bin/aarch64-linux-gnu-
 export CROSS_COMPILE=$BUILD_CROSS_COMPILE
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 export USE_CCACHE=1
