1134c72cb0ce058a784a1da5a444ec7b245fd6a2
enesuzun2002 <enesuzun200227@gmail.com>
2020-03-27 21:52:54 +0300

    Revert "dm_cpu_hotplug: remove in_delay & out_delay"
    This reverts commit 1d52894bfd048475356f6f272f17b4692ef3d47c.

--- drivers/cpufreq/dm_cpu_hotplug.c
+++ drivers/cpufreq/dm_cpu_hotplug.c
@@ -110,6 +110,8 @@ static void calc_load(void);
 static enum hotplug_cmd prev_cmd = CMD_NORMAL;
 static enum hotplug_cmd exe_cmd;
 static unsigned int delay = POLLING_MSEC;
+static unsigned int out_delay = POLLING_MSEC;
+static unsigned int in_delay = POLLING_MSEC;
 
 #if defined(CONFIG_SCHED_HMP)
 static struct workqueue_struct *hotplug_wq;
@@ -312,23 +314,31 @@ static ssize_t store_stay_threshold(struct kobject *kobj, struct attribute *attr
 static ssize_t show_dm_hotplug_delay(struct kobject *kobj,
 				struct attribute *attr, char *buf)
 {
-	return snprintf(buf, PAGE_SIZE, "%u\n", delay);
+	return snprintf(buf, PAGE_SIZE, "hotplug delay (out : %umsec, in : %umsec, cur : %umsec)\n",
+				out_delay, in_delay, delay);
 }
 
 static ssize_t store_dm_hotplug_delay(struct kobject *kobj, struct attribute *attr,
 					const char *buf, size_t count)
 {
-	int input_delay;
+	int input_out_delay, input_in_delay;
 
-	if (!sscanf(buf, "%8d", &input_delay))
+	if (!sscanf(buf, "%8d %8d", &input_out_delay, &input_in_delay))
 		return -EINVAL;
 
-	if (input_delay < 0) {
-		pr_err("%s: invalid value (%d)\n", __func__, input_delay);
+	if (input_out_delay < 0 || input_in_delay < 0) {
+		pr_err("%s: invalid value (%d, %d)\n",
+			__func__, input_out_delay, input_in_delay);
 		return -EINVAL;
 	}
 
-	delay = (unsigned int)input_delay;
+	out_delay = (unsigned int)input_out_delay;
+	in_delay = (unsigned int)input_in_delay;
+
+	if (in_low_power_mode)
+		delay = in_delay;
+	else
+		delay = out_delay;
 
 	return count;
 }
@@ -717,6 +727,7 @@ static int dynamic_hotplug(enum hotplug_cmd cmd)
 	case CMD_LOW_POWER:
 		ret = __cpu_hotplug(true, cmd);
 		in_low_power_mode = true;
+		delay = in_delay;
 		break;
 	case CMD_CLUST0_ONE_OUT:
 	case CMD_CLUST1_OUT:
@@ -731,6 +742,7 @@ static int dynamic_hotplug(enum hotplug_cmd cmd)
 	case CMD_NORMAL:
 		ret = __cpu_hotplug(false, cmd);
 		in_low_power_mode = false;
+		delay = out_delay;
 		break;
 	}
 
