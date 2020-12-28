dbd652708ce17d3a64cffff79edf26a0648ad3a2
enesuzun2002 <enesmuratuzun@outlook.com.tr>
2020-12-28 22:37:48 +0300

    cpufreq: undervolted cpus by 62.5mv
    * Removed non-used EXYNOS7420-OVER_UNDER-CLOCK definitions

--- drivers/cpufreq/exynos7420-atlas-cpufreq.c
+++ drivers/cpufreq/exynos7420-atlas-cpufreq.c
@@ -127,21 +127,10 @@ static struct apll_freq exynos7420_apll_freq_CA57[] = {
  * ASV group voltage table
  */
 static const unsigned int asv_voltage_7420_CA57[CPUFREQ_LEVEL_END_CA57] = {
-#ifdef CONFIG_EXYNOS7420_OVERCLOCK
-	1450000,	/* L0  2500 */
-	1400000,	/* L1  2400 */
-	1350000,	/* L2  2300 */
-	1300000,	/* L3  2200 */
-	1375000,	/* L0  2500 */
-	1325000,	/* L1  2400 */
-	1275000,	/* L2  2300 */
-	1225000,	/* L3  2200 */
-#else
 	1250000,	/* L0  2500 */
 	1250000,	/* L1  2400 */
 	1250000,	/* L2  2300 */
 	1250000,	/* L3  2200 */
-#endif
 	1212500,	/* L4  2100 */
 	1162500,	/* L5  2000 */
 	1118750,	/* L6  1900 */
