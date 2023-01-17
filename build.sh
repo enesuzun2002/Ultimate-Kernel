8bbf425b4759ffb11929be633ec129e9e9e2bcda
enesuzun2002 <emurat.uzun@gmail.com>
2023-01-18 00:27:27 +0300

    defconfig: swith to BFQ io-scheduler

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@
 # -----
 export ARCH=arm64
 export SUBARCH=arm64
-export BUILD_CROSS_COMPILE=/home/emurat_uzun/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export BUILD_CROSS_COMPILE=/home/emurat/Projects/android/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export CROSS_COMPILE=$BUILD_CROSS_COMPILE
 export BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 export USE_CCACHE=1
